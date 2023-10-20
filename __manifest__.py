96dd9565e4417da242020750be7882ca71a60730
root <hoda.afrt@yahoo.com>
2023-10-20 18:30:13 +0300

    Updating Mainfeist

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,15 @@
+
 # -*- coding: utf-8 -*-
 {
     'name': 'Customer And Vendor Indexing',
-    'version': '1.0',
-    'author': 'Eng / Mahmoud Ossman',
+    'version': '16.0.1.0.0',
+    'author': 'Vipdoo - Eng / Mahmoud Ossman',
     'category': 'Contacts, Products',
+    'summary': 'Contact Us : +201222842875',
+    'license': 'LGPL-3',
+    'website': 'https://vipdooegypt.com/',
     'depends': ['base', 'mail', 'account', 'contacts', 'sale'],
-    'description': """ """,
+    'description': """ Adding Sequance For Customers And Vendors """,
     'data': [
         'views/res_partner_views.xml',
         'views/account_move.xml',
